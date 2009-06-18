bb1cd8edda692b3b42cd9062e74626524d0c0afd
Luke Daley <ld@ldaley.com>
2009-06-18 01:41:28 +0000

    Cleaned up whitespace and imports.

--- spepintegrators/grails/src/groovy/grails/spep/SpepEnabledJettyServerFactory.groovy
+++ spepintegrators/grails/src/groovy/grails/spep/SpepEnabledJettyServerFactory.groovy
@@ -5,12 +5,11 @@ import grails.web.container.EmbeddableServer
 
 public class SpepEnabledJettyServerFactory implements EmbeddableServerFactory {
 
+	public EmbeddableServer createInline(String basedir, String webXml, String contextPath, ClassLoader classLoader) {
+		new SpepEnabledJettyServer(basedir, webXml, contextPath, classLoader)
+	}
 
-    public EmbeddableServer createInline(String basedir, String webXml, String contextPath, ClassLoader classLoader) {
-        return new SpepEnabledJettyServer(basedir, webXml, contextPath, classLoader)
-    }
-
-    public EmbeddableServer createForWAR(String warPath, String contextPath) {
-        return new SpepEnabledJettyServer(warPath, contextPath)
-    }
+	public EmbeddableServer createForWAR(String warPath, String contextPath) {
+		new SpepEnabledJettyServer(warPath, contextPath)
+	}
 }

2621707bad7e62d5b453fcdf5796ec669b70c2ed
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2009-11-23 09:26:45 +1000

    Changed to generate keystore for unit tests.

--- esoessoplugins/artifact/tests/regression/com/qut/middleware/esoe/sso/plugins/artifact/ArtifactDaoTest.java
+++ esoessoplugins/artifact/tests/regression/com/qut/middleware/esoe/sso/plugins/artifact/ArtifactDaoTest.java
@@ -1,20 +1,20 @@
 /* Copyright 2008, Queensland University of Technology
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not 
- * use this file except in compliance with the License. You may obtain a copy of 
- * the License at 
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations under 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * Author: Shaun Mangelsdorf
  * Creation Date: 17/12/2008
- * 
- * Purpose: 
+ *
+ * Purpose:
  */
 
 package com.qut.middleware.esoe.sso.plugins.artifact;
@@ -51,14 +51,14 @@ import com.qut.middleware.saml2.schemas.soap.v12.Envelope;
 
 public class ArtifactDaoTest
 {
-	
-	private String keystorePath = "tests/testdata/testKeystore.ks";
+
+	private String keystorePath = "build/testdata/testKeystore.ks";
 	private String keystorePassword = "kspass";
 	private String keyPassword = "keypass";
 	private String keyAlias = "testcert";
 
 	private KeystoreResolver keystoreResolver;
-	
+
 	byte[] sourceID = new byte[]{
 			0, 0, 0, 0, 0,
 			0, 0, 0, 0, 0,
@@ -71,7 +71,7 @@ public class ArtifactDaoTest
 			1, 2, 3, 4, 5,
 			1, 2, 3, 4, 5,
 	};
-	
+
 	public ArtifactDaoTest() throws Exception
 	{
 		this.keystoreResolver = new KeystoreResolverImpl(new File(keystorePath), keystorePassword, "testpriv", keyPassword);
@@ -85,16 +85,16 @@ public class ArtifactDaoTest
 		Artifact artifact = new Artifact(0, sourceID, messageHandle, audience, document);
 		ArtifactDao artifactDao = new ArtifactDaoMemoryImpl();
 		artifactDao.storeArtifact(artifact);
-		
+
 		String artifactToken = artifact.toBase64Artifact();
-		
+
 		Artifact artifactRetrieved = new Artifact(artifactToken);
 		artifactDao.resolveArtifact(artifactRetrieved);
-		
+
 		assertEquals(audience, artifactRetrieved.getAudience());
 		assertEquals(document, artifactRetrieved.getDocument());
 	}
-	
+
 	@Test
 	public void testSignature() throws Exception
 	{
@@ -102,16 +102,16 @@ public class ArtifactDaoTest
 		String[] schemas = new String[]{SchemaConstants.samlProtocol};
 		String soapPackage = Envelope.class.getPackage().getName();
 		String[] soapSchemas = new String[]{SchemaConstants.soapv12};
-		
+
 		Marshaller<Response> marshaller = new MarshallerImpl<Response>(packages, schemas, this.keystoreResolver);
 		Marshaller<ArtifactResponse> artifactMarshaller = (Marshaller)marshaller;
 		Unmarshaller<Response> unmarshaller = new UnmarshallerImpl<Response>(packages, schemas, this.keystoreResolver);
 		Unmarshaller<ArtifactResponse> artifactUnmarshaller = (Unmarshaller)unmarshaller;
 		Marshaller<Envelope> soapMarshaller = new MarshallerImpl<Envelope>(soapPackage, soapSchemas, this.keystoreResolver);
 		Unmarshaller<Envelope> soapUnmarshaller = new UnmarshallerImpl<Envelope>(soapPackage, soapSchemas, this.keystoreResolver);
-		
+
 		byte[] doc = null;
-		
+
 		{
 			Response response = new Response();
 			response.setSignature(new Signature());
@@ -122,9 +122,9 @@ public class ArtifactDaoTest
 			response.getStatus().setStatusMessage("message");
 			response.getStatus().setStatusCode(new StatusCode());
 			response.getStatus().getStatusCode().setValue(StatusCodeConstants.success);
-			
+
 			Element element1 = marshaller.marshallSignedElement(response);
-			
+
 			ArtifactResponse artifactResponse = new ArtifactResponse();
 			artifactResponse.setSignature(new Signature());
 			artifactResponse.setID("Efgh");
@@ -134,18 +134,18 @@ public class ArtifactDaoTest
 			artifactResponse.getStatus().setStatusMessage("message");
 			artifactResponse.getStatus().setStatusCode(new StatusCode());
 			artifactResponse.getStatus().getStatusCode().setValue(StatusCodeConstants.success);
-			
+
 			artifactResponse.setAny(element1);
-	
+
 			Element artifactResponseElement = artifactMarshaller.marshallSignedElement(artifactResponse);
-			
+
 			Envelope envelope = new Envelope();
 			envelope.setBody(new Body());
 			envelope.getBody().getAnies().add(artifactResponseElement);
-			
+
 			doc = soapMarshaller.marshallUnSigned(envelope);
 		}
-		
+
 		Envelope envelope = soapUnmarshaller.unMarshallUnSigned(doc);
 		ArtifactResponse artifactResponse = artifactUnmarshaller.unMarshallUnSigned(envelope.getBody().getAnies().get(0));
 		Response response = unmarshaller.unMarshallSigned(artifactResponse.getAny());

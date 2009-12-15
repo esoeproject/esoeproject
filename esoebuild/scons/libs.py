917ea0b8a8d9937cbcc18c096d7f8bd209f1d849
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2009-12-15 17:13:12 +1000

    Renamed library functions to make it clearer that the libraries are being included.

--- esoebuild/scons/libs.py
+++ esoebuild/scons/libs.py
@@ -3,31 +3,45 @@ Import('conf')
 class libs:
 	conf
 	boost_suffix = ''
+	boost_libraries = ['program_options', 'thread', 'date_time', 'regex']
 
 	def __init__(self, conf):
 		self.conf = conf
 		if ARGUMENTS.get('boostsuffix'): self.boost_suffix = ARGUMENTS.get('boostsuffix')
 
-	def check_xsd_lib(self):
-		if not self.conf.CheckCXXHeader('xsd/cxx/tree/elements.hxx'):
+	def xsd_lib(self):
+		if not conf.CheckCXXHeader('xsd/cxx/tree/elements.hxx'):
 			print 'Did not find XSD library'
 			Exit(1)
 
-	def check_xerces_lib(self):
-		if not self.conf.CheckLibWithHeader('xerces-c', 'xercesc/dom/DOMElement.hpp', 'c++'):
+	def xerces_lib(self):
+		if not conf.CheckLibWithHeader('xerces-c', 'xercesc/dom/DOMElement.hpp', 'c++'):
 			print 'Did not find Xerces library'
 			Exit(1)
 
-	def check_xml_security_lib(self):
-		if not self.conf.CheckLibWithHeader('xml-security-c', 'xsec/utils/XSECPlatformUtils.hpp', 'c++'):
+	def xml_security_lib(self):
+		if not conf.CheckLibWithHeader('xml-security-c', 'xsec/utils/XSECPlatformUtils.hpp', 'c++'):
 			print 'Did not find Xml-Security-C library'
 			Exit(1)
 
-	def check_boost_libs(self):
-		boost_libs = ['program_options', 'thread', 'date_time']
-
-		if False in [self.conf.CheckLibWithHeader('boost_'+name+self.boost_suffix, 'boost/'+name+'.hpp', 'c++') for name in boost_libs]:
+	def boost_libs(self):
+		if False in [conf.CheckLibWithHeader('boost_'+name+self.boost_suffix, 'boost/'+name+'.hpp', 'c++') for name in self.boost_libraries]:
 			print 'Did not find Boost libraries'
+			Exit(1)
+
+	def curl_lib(self):
+		if not conf.CheckLibWithHeader('curl', 'curl/curl.h', 'c'):
+			print 'Did not find Curl library'
+			Exit(1)
+
+	def asio_lib(self):
+		if not conf.CheckCXXHeader('asio.hpp'):
+			print 'Did not find Asio library'
+			Exit(1)
+
+	def esoe_saml_lib(self):
+		if not conf.CheckLibWithHeader(['esoesaml2'] + [name+self.boost_suffix for name in self.boost_libraries], 'saml2/SAML2Defs.h', 'c++'):
+			print 'Did not find ESOE SAML 2.0 library'
 			Exit(1)
 
 obj = libs(conf)

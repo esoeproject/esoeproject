f91d27cbb23a0d0433c7ec234a493b1359961693
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2009-12-22 11:53:06 +1000

    Fixed Win32 binary linking to embed manifest properly.

--- esoebuild/scons/env.py
+++ esoebuild/scons/env.py
@@ -10,8 +10,10 @@ env = Environment(CPPPATH=includedirs, LIBPATH=libdirs, PLATFORM=platform)
 env.ParseFlags('-Optimized')
 if platform == 'win32':
 	env.MergeFlags('-DWIN32 -D_WINDOWS -D_WIN32_WINNT=0x0501')
-	env.MergeFlags({'CCFLAGS':Split('/MD /EHsc /W0 /nologo /Ox')})
-	#env.MergeFlags({'LINKFLAGS':['/FORCE:MULTIPLE']})
+	env.MergeFlags({'CCFLAGS':Split('/MD /TP /EHsc /W0 /nologo /Ox')})
+	env['LINKCOM'] = [env['LINKCOM'], 'mt.exe -nologo -manifest ${TARGET}.manifest -outputresource:${TARGET};#1']
+	env['SHLINKCOM'] = [env['SHLINKCOM'], 'mt.exe -nologo -manifest ${TARGET}.manifest -outputresource:${TARGET};#2']
+
 env.VariantDir('build', 'src', duplicate=0)
 
 Return('env')

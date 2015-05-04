b3c3741cf5d64af06875560bd42051c679fd686f
Andrew McGregor <ap.mcgregor@qut.edu.au>
2015-05-04 11:12:41 +1000

    Updated with Stephen T Lavalej's version..

--- saml2cpp/include/saml2/SAML2Defs.h
+++ saml2cpp/include/saml2/SAML2Defs.h
@@ -30,19 +30,27 @@ template class __declspec(dllexport) std::basic_string<wchar_t, std::char_traits
 #endif
 
 #ifdef __GNUC__
+#include <memory>
 // GCC 4.8+ only supports C++11. std::make_unique is a C++14 feature and is only supported in GCC 4.9+
 namespace std {
 
-	template<typename T, typename A1>
-	std::unique_ptr<T> make_unique(A1&& a1)
-	{
-		return std::unique_ptr<T>(new T(std::forward<A1>(a1)));
+	template <typename T, typename... Args>
+	std::unique_ptr<T> make_unique_helper(std::false_type, Args&&... args) {
+		return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 	}
 
-	template<typename T, typename A1, typename A2>
-	std::unique_ptr<T> make_unique(A1&& a1, A2&& a2)
-	{
-		return std::unique_ptr<T>(new T(std::forward<A1>(a1), std::forward<A2>(a2)));
+	template <typename T, typename... Args>
+	std::unique_ptr<T> make_unique_helper(std::true_type, Args&&... args) {
+		static_assert(std::extent<T>::value == 0,
+			"make_unique<T[N]>() is forbidden, please use make_unique<T[]>().");
+
+		typedef typename std::remove_extent<T>::type U;
+		return std::unique_ptr<T>(new U[sizeof...(Args)]{std::forward<Args>(args)...});
+	}
+
+	template <typename T, typename... Args>
+	std::unique_ptr<T> make_unique(Args&&... args) {
+		return make_unique_helper<T>(std::is_array<T>(), std::forward<Args>(args)...);
 	}
 
 }

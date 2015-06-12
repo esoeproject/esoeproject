2c4cc373384084fa3e76b8bf9b6578e381b3bbe9
unknown <kaurm@XXXX-YYY-071614.qut.edu.au>
2015-06-12 16:58:47 +1000

    Changes for Http Request Filter

---
+++ spepiismodule/src/HttpRequest.cpp
@@ -0,0 +1,280 @@
+#include "HttpRequest.h"
+#include "FilterConstants.h"
+
+#include "spep/exceptions/InvalidStateException.h"
+
+#include <ctype.h>
+
+#include <boost/lexical_cast.hpp>
+
+// This is only safe for direct variable parameters
+// Any expression with a side effect, such as a ++ or a function call should NOT be passed into this macro.
+#define URLDECODE_HEX2DEC(x) ( ( x <= 'f' && x >= 'a' ) ? ( 10 + x - 'a' ) : ( x <= '9' && x >= '0' ) ? ( x - '0' ) : -1 )
+
+namespace spep {
+	namespace isapi {
+
+		HttpRequestImpl::HttpRequestImpl(IHttpContext * pHttpContext) :
+			httpContext(pHttpContext),
+			mRequestURL(),
+			mRequestMethod(pHttpContext->GetRequest()->GetHttpMethod()),
+			mQueryString(pHttpContext->GetRequest()->GetRawHttpRequest()->CookedUrl.pQueryString),
+			mScriptName(),
+			mContentType(),
+			mContentLength(0),
+			mHeadersSent(false)
+		{
+			mRequestURL = getServerVariable("URL");
+			mScriptName = getServerVariable("SCRIPT_NAME");
+			mContentType = getServerVariable("CONTENT_TYPE");
+			mRemoteAddress = getServerVariable("REMOTE_ADDR");
+			mIsSecureRequest = (getServerVariable("SERVER_PORT_SECURE").compare(std::wstring(L"1")) == 0);
+			try
+			{
+				mContentLength = boost::lexical_cast<DWORD>(getServerVariable("CONTENT_LENGTH"));
+			}
+			catch (boost::bad_lexical_cast&)
+			{
+			}
+		}
+
+		HttpRequestImpl::~HttpRequestImpl(){
+			for (auto iter = mFreeList.begin(); iter != mFreeList.end(); ++iter)
+			{
+				free(*iter);
+			}
+		}
+
+		std::string HttpRequestImpl::getHeader(const std::string &name){
+			std::string header;
+
+			PCSTR headerValue;
+			USHORT buf;
+			headerValue = httpContext->GetHeader(name, &buf);
+			if (buf) {
+				header = std::string(headerValue);
+			}
+			return header;
+		}
+
+		void HttpRequestImpl::setHeader(const std::string& headerName, const std::string& headerValue){
+			if (mHeadersSent)
+				throw spep::InvalidStateException();
+
+			mResponseHeaders.insert(std::make_pair<std::string, std::string>(headerName, headerValue));
+		}
+
+		std::wstring HttpRequestImpl::getServerVariable(const std::string& name){
+
+			// Create an HRESULT to receive return values from methods.
+			HRESULT hr;
+
+			DWORD size = 128;
+			std::wstring variableValue;
+			PCWSTR returnString;
+
+			hr = httpContext->GetServerVariable(name.c_str(), &returnString, &size);
+
+			if (!FAILED(hr)){
+				variableValue = std::wstring(returnString);
+			}
+			return variableValue;
+		}
+
+		IHttpContext * HttpRequestImpl::getHttpContext(){
+			return httpContext;
+		}
+
+		std::wstring HttpRequestImpl::getRequestURL() const
+		{
+			return mRequestURL;
+		}
+
+		std::string HttpRequestImpl::getRequestMethod() const
+		{
+			return mRequestMethod;
+		}
+
+		std::string HttpRequestImpl::getQueryString() const
+		{
+			return mQueryString;
+		}
+
+		std::wstring HttpRequestImpl::getScriptName() const
+		{
+			return mScriptName;
+		}
+
+		std::wstring HttpRequestImpl::getContentType() const
+		{
+			return mContentType;
+		}
+
+		DWORD HttpRequestImpl::getContentLength() const
+		{
+			return mContentLength; //ask why?
+		}
+
+		std::wstring HttpRequestImpl::getRemoteAddress() const
+		{
+			return mRemoteAddress;
+		}
+
+		BOOL HttpRequestImpl::isSecureRequest() const
+		{
+			return mIsSecureRequest;
+		}
+
+		IHttpContext *HttpRequestImpl::getHttpContext()
+		{
+			return httpContext; //check if this is correct
+		}
+
+		void HttpRequestImpl::setRemoteUser(const std::string& username)
+		{
+			mRemoteUser = username;
+		}
+
+		void HttpRequestImpl::setRemoteAddress(const std::wstring& ipaddress)
+		{
+			mRemoteAddress = ipaddress;
+		}
+
+
+		DWORD HttpRequestImpl::sendResponseHeader(int statuscode, const std::string& statusLine, BOOL keepConn)
+		{
+			if (mHeadersSent) throw spep::InvalidStateException();
+			HRESULT hr;
+			BOOL errorOccured = false;
+
+			for (const auto& it: mResponseHeaders)
+			{
+				hr = httpContext->GetResponse()->SetHeader(it.first.c_str(),it.second.c_str(), (USHORT)(it.second.size()), true);
+				if (FAILED(hr)){
+					errorOccured = true;
+				}
+			}
+
+			hr = httpContext->GetResponse()->SetStatus(statuscode, statusLine.c_str(), 0, S_OK);
+			if (FAILED(hr)){
+				errorOccured = true;
+			}
+			if (errorOccured){
+				return HSE_STATUS_ERROR;
+			}
+			else{
+				mHeadersSent = true;
+			}
+			return HSE_STATUS_SUCCESS;
+		}
+
+		DWORD HttpRequestImpl::sendResponseDocument(int statuscode, const std::string& statusLine, const char *document, DWORD documentLength, const std::string& contentType)
+		{
+			HRESULT hr;
+
+			setHeader(CONTENT_TYPE_HEADER, contentType);
+			setHeader(CONTENT_LENGTH_HEADER, std::to_string(documentLength));
+
+			if (!mHeadersSent)
+			{
+				DWORD result = sendResponseHeader(statuscode, statusLine);
+				if (result != HSE_STATUS_SUCCESS)
+					return result;
+			}
+
+			HTTP_DATA_CHUNK dc;
+			dc.DataChunkType = HttpDataChunkFromMemory;
+			dc.FromMemory.BufferLength = documentLength;
+			dc.FromMemory.pBuffer = httpContext->AllocateRequestMemory(documentLength + 1);
+
+			if (!dc.FromMemory.pBuffer){
+				return HSE_STATUS_ERROR;
+			}
+			char *p = static_cast<char *>(dc.FromMemory.pBuffer);
+			strcpy_s(p, documentLength + 1, document);
+
+			hr = httpContext->GetResponse()->WriteEntityChunkByReference(&dc, -1);
+
+			if (FAILED(hr)){
+				return HSE_STATUS_ERROR;
+			}
+			return HSE_STATUS_SUCCESS;
+		}
+
+		VOID* HttpRequestImpl::allocMem(DWORD size)
+		{
+			LPVOID retval = malloc(size);
+			mFreeList.push_back(retval);
+			return retval;
+		}
+
+		char* HttpRequestImpl::istrndup(const char *str, size_t len)
+		{
+			char *dst = static_cast<char*>(allocMem(len + 1));
+			std::strncpy(dst, str, len);
+			dst[len] = '\0';
+			return dst;
+		}
+
+		char *HttpRequestImpl::isprintf(const char *fmt, ...)
+		{
+			int size = strlen(fmt) + 1;
+			CArray<char> dst(size);
+
+			// Yeah, but see below
+			while (1)
+			{
+				int written;
+				va_list vargs;
+				va_start(vargs, fmt);
+				written = vsnprintf(dst.get(), size, fmt, vargs);
+				va_end(vargs);
+
+				// We won't loop, because "size" is always growing, so eventually it will be big enough.
+				if (written < 0)
+				{
+					size = 2 * size;
+					dst.resize(size);
+				}
+				else
+				{
+					return istrndup(dst.get(), written);
+				}
+			}
+		}
+
+		void HttpRequestImpl::urlDecode(std::string& url)
+		{
+			// This algorithm adapted from Apache httpd(trunk-2.3) server/util.c 'unescape_url'
+
+			std::size_t i, j;
+			// Jump to the first % sign ... we don't need to process anything before that.
+			i = url.find_first_of('%');
+
+			for (j = i; i < url.length(); ++i, ++j)
+			{
+				if (url[i] != '%')
+				{
+					url[j] = url[i];
+				}
+				else
+				{
+					char hi_ = tolower(url[i + 1]);
+					char lo_ = tolower(url[i + 2]);
+					int hi = URLDECODE_HEX2DEC(hi_);
+					int lo = URLDECODE_HEX2DEC(lo_);
+					if (hi == -1 || lo == -1)
+					{
+						url[j] = url[i];
+					}
+					else
+					{
+						url[j] = (0x10 * hi) + lo;
+						i += 2;
+					}
+				}
+			}
+			url = url.substr(0, j);
+		}
+	}
+}
---
+++ spepiismodule/src/HttpRequest.h
@@ -0,0 +1,111 @@
+#ifndef HTTPREQUEST_H_
+#define HTTPREQUEST_H_
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+
+#define HTTP_HEADER_VALUE_DEFAULT_LENGTH 64
+#define HTTP_HEADER_VARIABLE_PREFIX "HTTP_"
+
+#include <winsock2.h>
+#include <windows.h>
+#include <httpfilt.h>
+#include <httpext.h>
+#include <httpserv.h>
+
+#include "spep/Util.h"
+
+#include "FilterConstants.h"
+
+namespace spep{
+	namespace isapi{
+		class HttpRequest
+		{
+		public:
+			virtual ~HttpRequest(){}
+			virtual std::string getHeader(const std::string &name) = 0;
+			virtual void setHeader(const std::string& headerName, const std::string& headerValue) = 0;
+			virtual std::wstring getServerVariable(const std::string& name) = 0;
+			virtual std::string getRequestMethod() const = 0;
+			virtual std::string getQueryString() const = 0;
+			virtual std::string getScriptName() const = 0;
+			virtual std::string getRequestURL() const = 0;
+			virtual std::string getContentType() const = 0;
+			virtual std::string getRemoteAddress() const = 0;
+			virtual DWORD getContentLength() const = 0;
+			virtual BOOL isSecureRequest() const = 0;
+			virtual IHttpContext *getHttpContext() = 0;
+			virtual DWORD sendResponseHeader(int statuscode, const std::string& statusLine, BOOL keepConn = FALSE) = 0;
+			virtual DWORD sendResponseDocument(int statuscode, const std::string& statusLine, const char *document, DWORD documentLength, const std::string& contentType) = 0;
+			virtual DWORD sendErrorDocument(int errorCode, int minorCode = 0) = 0;
+			virtual DWORD sendRedirectResponse(const std::string& location) = 0;
+			virtual BOOL readRequestDocument(spep::CArray<char> &buffer, DWORD &size) = 0;
+			virtual VOID* allocMem(DWORD size) = 0;
+			virtual char* istrndup(const char *str, size_t len) = 0;
+			virtual char *isprintf(const char *fmt, ...) = 0;
+			virtual void addRequestHeader(const std::string& name, const std::string& value) = 0;
+			virtual DWORD continueRequest() = 0;
+			virtual void urlDecode(std::string& url) = 0;
+			virtual void setRemoteUser(const std::string& username) = 0;
+			virtual void setRemoteAddress(const std::string& ipaddress) = 0;
+			
+		};
+
+		class HttpRequestImpl : public HttpRequest
+		{
+			HttpRequestImpl(IHttpContext *pHttpContext);
+			
+			virtual ~HttpRequestImpl();
+
+			virtual std::string getHeader(const std::string &name) override;
+			virtual void setHeader(const std::string& headerName, const std::string& headerValue) override;
+			virtual std::wstring getServerVariable(const std::string& name) override;
+			virtual std::string getRequestURL() const override;
+			virtual std::string getRequestMethod() const override;
+			virtual std::string getQueryString() const override;
+			virtual std::string getScriptName() const override;
+			virtual std::string getContentType() const override;
+			virtual std::string getRemoteAddress() const override;
+			virtual DWORD getContentLength() const override;
+			virtual BOOL isSecureRequest() const override;
+			virtual IHttpContext *getHttpContext() override;
+			virtual void setRemoteUser(const std::string& username) override;
+			virtual void setRemoteAddress(const std::string& ipaddress) override;
+
+			virtual DWORD sendResponseHeader(int statuscode, const std::string& statusLine, BOOL keepConn = FALSE) override;
+			virtual DWORD sendResponseDocument(int statuscode, const std::string& statusLine, const char *document, DWORD documentLength, const std::string& contentType) override;
+			virtual DWORD sendErrorDocument(int errorCode, int minorCode = 0) override;
+			virtual DWORD sendRedirectResponse(const std::string& location) override;
+			virtual BOOL readRequestDocument(spep::CArray<char> &buffer, DWORD &size) override;
+			virtual void addRequestHeader(const std::string& name, const std::string& value) override;
+
+			virtual VOID* allocMem(DWORD size) override;
+			virtual char* istrndup(const char *str, size_t len) override;
+			virtual char *isprintf(const char *fmt, ...) override;
+			virtual DWORD continueRequest() override;
+			virtual void urlDecode(std::string& url) override;
+
+			
+
+		private:
+			IHttpContext * httpContext;
+			std::vector<LPVOID> mFreeList;
+			std::unordered_map<std::string, std::string> mResponseHeaders;
+			std::wstring mRequestURL;
+			std::string mRequestMethod;
+			std::string mQueryString;
+			std::wstring mScriptName;
+			std::wstring mContentType;
+			std::string mRemoteUser;
+			std::wstring mRemoteAddress;
+			std::string mChildHeaders;
+			BOOL mIsSecureRequest;
+			DWORD mContentLength;
+			bool mHeadersSent;
+
+		};
+	}
+}
+#endif /*HTTPREQUEST_H_*/
